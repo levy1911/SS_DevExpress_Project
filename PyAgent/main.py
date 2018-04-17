200791ecdf0f0b8769db242bd44196e9e785abec
levy1911 <lewpaw@gmail.com>
2018-04-17 10:25:54 +0200

    Development process...

--- PyAgent/main.py
+++ PyAgent/main.py
@@ -8,7 +8,8 @@ gene_modules = ['SoftMol', 'SoftDxp', 'SoftFlw', 'SoftBio', 'SoftCtg', 'SoftAcc'
                 'SoftBioChem', 'SoftPathDx']
 
 line_length = 150
-
+# Final results output
+final_output = ''
 with Popen(['java', '-jar', file_path], stdout=PIPE, bufsize=1, universal_newlines=True) as p:
     env_name = ''
     env_dict = {}
@@ -28,8 +29,6 @@ with Popen(['java', '-jar', file_path], stdout=PIPE, bufsize=1, universal_newlin
             module_list.append(temp_list)
 
         env_dict[current[0]] = module_list
-
-
 
     modules_tmp = []
     modules_per_env = []
@@ -51,145 +50,139 @@ with Popen(['java', '-jar', file_path], stdout=PIPE, bufsize=1, universal_newlin
             modules_tmp.append(item[1])
             modules_per_env = set(modules_tmp)
 
-
-        #Clear list from non-gene, non-checkable items
+        # Clear list from non-gene, non-checkable items
         for i in reversed(v):
-            if (i[3] not in modules_per_env) and ('DevExpress' not in i[3]):
+            if (i[3] not in modules_per_env) and (analyze_module not in i[3]):
                 v.remove(i)
-
 
         # All DevExpress -ends positions
         analyzed_module_ends_list = [x for x in v if analyze_module in x[3]]
 
         # Not displaying environments with no DevExpress:
-        if len(analyzed_module_ends_list) == 0:
-            break
+        if len(analyzed_module_ends_list) != 0:
 
-        gene_modules_per_env = [x for x in modules_per_env if x in gene_modules]
+            gene_modules_per_env = [x for x in modules_per_env if x in gene_modules]
 
-        print('\nENVIRONMENT: ' + str(key) + '\n\n')
+            final_output += '\nENVIRONMENT: ' + str(key) + '\n\n'
 
+            analyzed_module_ends_list_unique = []
 
-        analyzed_module_ends_list_unique = []
+            final_output += '\nInvolved modules: \n\n'
+            for item in analyzed_module_ends_list:
+                analyzed_module_ends_list_unique.append(str(item[3]) + ' ' + str(item[4]))
+                final_output += ' - ' + str(item[1]) + ' ' + str(item[2]) + '\n'
 
-        print('Involved modules:')
-        for item in analyzed_module_ends_list:
-            analyzed_module_ends_list_unique.append(str(item[3]) + ' ' + str(item[4]))
-            print(' - ' +str(item[1]) + ' ' + str(item[2]))
+            final_output += "\nDevExpress versions on environment: \n\n"
+            analyzed_module_ends_list_unique = set(analyzed_module_ends_list_unique)
+            for item in analyzed_module_ends_list_unique:
+                final_output += ' - ' + str(item) + '\n'
+            if len(analyzed_module_ends_list_unique) > 1:
+                affected_environments.append(str(key)+'('+str(len(analyzed_module_ends_list_unique))+')')
 
-        print("\nDevExpress versions on environment: ")
-        analyzed_module_ends_list_unique = set(analyzed_module_ends_list_unique)
-        for item in analyzed_module_ends_list_unique:
-            print(' - ' + str(item))
-        if len(analyzed_module_ends_list_unique) > 1:
-            affected_environments.append(str(key)+'('+str(len(analyzed_module_ends_list_unique))+')')
-        print('\n')
+            final_output += '\nGene modules on Environments:\n\n'
+            for item in gene_modules_per_env:
+                final_output += ' - ' + str(item) + '\n'
 
-        print('Gene modules on Environments:\n')
-        for item in gene_modules_per_env:
-            print(' - '+ str(item))
-
-        print('\nGene modules indirect connections with DevExpress: \n')
-        tmp = []
-        tmp2 = []
-        for item in analyzed_module_ends_list:
-            tmp = [x+[item[5]] for x in v if item[1] in x[3]]
-            tmp2.append(tmp)
-
-        # make one list
-        list_1 = []
-        for x in tmp2:
-            for item in x:
-                if item[1] in gene_modules:
-                    candidates_tmp.append(item)
-                else:
-                    list_1.append(item)
-
-        ##########START#############
-        tmp2 = []
-        for item in list_1:
-            #print(item[6])
-            tmp = [x+[item[5], item[6]] for x in v if item[1] in x[3]]
-            tmp2.append(tmp)
-
-        # make one list
-        list_2 = []
-        for x in tmp2:
-            for item in x:
-                if item[1] in gene_modules:
-                    candidates_tmp.append(item)
-                else:
-                    list_2.append(item)
-
-
-        ##########STOP############
-        if list_2 is not None:
+            final_output += '\nGene modules indirect connections with DevExpress: \n\n'
+            tmp = []
             tmp2 = []
-            for item in list_2:
-                tmp = [x + [item[5], item[6], item[7]] for x in v if item[1] in x[3]]
+            for item in analyzed_module_ends_list:
+                tmp = [x+[item[5]] for x in v if item[1] in x[3]]
                 tmp2.append(tmp)
 
             # make one list
-            list_3 = []
+            list_1 = []
             for x in tmp2:
                 for item in x:
                     if item[1] in gene_modules:
                         candidates_tmp.append(item)
                     else:
-                        list_3.append(item)
+                        list_1.append(item)
 
-        ##########STOP############
-        ##########START#############
-        if list_3 is not None:
+            ##########START#############
             tmp2 = []
-            for item in list_3:
-                tmp = [x + [item[5], item[6], item[7], item[8]] for x in v if item[1] in x[3]]
+            for item in list_1:
+                tmp = [x+[item[5], item[6]] for x in v if item[1] in x[3]]
                 tmp2.append(tmp)
 
             # make one list
-            list_4 = []
+            list_2 = []
             for x in tmp2:
                 for item in x:
                     if item[1] in gene_modules:
                         candidates_tmp.append(item)
                     else:
-                        list_4.append(item)
+                        list_2.append(item)
 
-        ##########STOP############
+            ##########STOP############
+            if list_2 is not None:
+                tmp2 = []
+                for item in list_2:
+                    tmp = [x + [item[5], item[6], item[7]] for x in v if item[1] in x[3]]
+                    tmp2.append(tmp)
 
-        indexes_list = []
-        inside_list = []
-        for item in candidates_tmp:
-            inside_list.append(item)
-            indexes_list = item[6:]
-            for i in v:
-                if i[5] in indexes_list:
-                    inside_list.append(i)
-            candidates_final.append(inside_list)
+                # make one list
+                list_3 = []
+                for x in tmp2:
+                    for item in x:
+                        if item[1] in gene_modules:
+                            candidates_tmp.append(item)
+                        else:
+                            list_3.append(item)
+
+            ##########STOP############
+            ##########START#############
+            if list_3 is not None:
+                tmp2 = []
+                for item in list_3:
+                    tmp = [x + [item[5], item[6], item[7], item[8]] for x in v if item[1] in x[3]]
+                    tmp2.append(tmp)
+
+                # make one list
+                list_4 = []
+                for x in tmp2:
+                    for item in x:
+                        if item[1] in gene_modules:
+                            candidates_tmp.append(item)
+                        else:
+                            list_4.append(item)
+
+            ##########STOP############
+
+            indexes_list = []
             inside_list = []
-        #print('candidate')
-        #for x in candidates_final:
-           # for i in x:
-            #    print(i)
-       # print('stop')
+            for item in candidates_tmp:
+                inside_list.append(item)
+                indexes_list = item[6:]
+                for i in v:
+                    if i[5] in indexes_list:
+                        inside_list.append(i)
+                candidates_final.append(inside_list)
+                inside_list = []
 
-        for i in candidates_final:
-            #Make string
-            indexes_list = i[0][5:]
-            final_string = 'GENE_MODULE: '
-            for item in indexes_list:
-                string = [x for x in v if x[5] == item]
-                final_string += str(string[0][1]) + '[ver: ' + (str(string[0][2]))+']'
-                if 'DevExpress' not in string[0][3]:
-                    final_string += ' --> '
-                elif 'DevExpress' in string[0][3]:
-                    final_string += ' --> ' + str(string[0][3]) + '[ver: ' + (str(string[0][4]))+']'
-            print(final_string)
-        print('-'*line_length)
-    print('#'*line_length)
-    print('Affected environments list (and how many different versions is detected):')
-    print(', '.join(affected_environments))
-    print('\n'+'#'*line_length)
+            for i in candidates_final:
+                #Make string
+                indexes_list = i[0][5:]
+                final_string = 'GENE_MODULE: '
+                for item in indexes_list:
+                    string = [x for x in v if x[5] == item]
+                    final_string += str(string[0][1]) + '[ver: ' + (str(string[0][2]))+']'
+                    if analyze_module not in string[0][3]:
+                        final_string += ' --> '
+                    elif analyze_module in string[0][3]:
+                        final_string += ' --> ' + str(string[0][3]) + '[ver: ' + (str(string[0][4]))+']'
+                final_output += final_string + '\n'
+            final_output += '-'*line_length +'\n'
+        else:
+            final_output += '\nENVIRONMENT: ' + str(key) + '\n\n'
+            final_output += '\n\nNO GENE MODULES\n\n'
+            final_output += '-' * line_length + '\n'
+    final_output += '#'*line_length + '\n'
+    final_output += 'Affected environments list (and how many different versions is detected):\n'
+    final_output += str(', '.join(affected_environments))
+    final_output += '\n'+'#'*line_length
+
+print(final_output)
 
 
 
