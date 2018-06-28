3a39e9c3ff666c77d55fc754d66875f201f4212b
levy1911 <lewpaw@gmail.com>
2018-06-28 10:54:17 +0200

    Update

--- PyAgent/main.py
+++ PyAgent/main.py
@@ -2,10 +2,10 @@ from seeker import Seeker
 from mail_module import send_mail
 
 s = Seeker()
-file_name = s.seek()
+file_name, second_file_name = s.seek()
 mail_text = 'DevExpress dependency report.'
 mail_title = 'DevExpress dependency report'
 send_to = ['pawelle@softsystem.pl']
 send_from = 'etools@softsystem.pl'
 
-#send_mail(send_from, send_to, mail_title, mail_text, [str(file_name)])
+#send_mail(send_from, send_to, mail_title, mail_text, [str(file_name), str(second_file_name)])
